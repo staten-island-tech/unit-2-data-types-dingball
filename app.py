b19cda879d3f005429d2ddb6f3ae656c333519c6
dingball <noobifiednooby@gmail.com>
2025-02-07 09:40:33 -0500

    tip calculator

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 def tip_calculate(service):
-    if service = "bad":
-print("0% tip")
-    elif service = "okay":
-print("15% tip")
-    elif service = "good":
-print("20% tip")
-    elif service = "great":
-print("25% tip")
+    if service == "bad":
+        print("0% tip")
+    elif service == "okay":
+        print("15% tip")
+    elif service == "good":
+        print("20% tip")
+    elif service == "great":
+        print("25% tip")
     else:
-print("Please enter a valid service rating")
+        print("Please enter a valid service rating")
 
-service = input("How was the service? (bad, okay, good, great): ")
+service = input("How was the service? (bad, okay, good, great):")
 tip_calculate(service)
-```
